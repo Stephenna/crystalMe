9868b77aee46020614be6c56cf8232d0c3c2bce9
Stephenna J Corry <Sjcstephenna@gmail.com>
2019-02-28 02:02:48 -0500

    needs to work on js

---
+++ script.js
@@ -0,0 +1,27 @@
+function determine(){
+ var opt = document.feeling.option1.value;
+ var points = 0;
+     
+    if (option1 == "overwhelmed"){
+        points = 1;
+    }
+    if (option1 == "stressed"){
+        points = 2;
+    }
+    if (option1 == "drained"){
+        points = 3;
+    }
+    if (option1 == "sleepy"){
+        points = 4;
+    }
+
+var messages = ["Blue Lace Agate is the stone of communication. If you're feeling buried, this crystal will bring you back to the surface. ",
+];
+var range =
+
+
+
+
+
+document.getElementById("after_submit").style.visibility = "visible";
+};
