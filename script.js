68a1007f541095368d42132f0fe70a92edc2c744
Stephenna J Corry <Sjcstephenna@gmail.com>
2019-03-03 21:14:17 -0500

    Working on rendering message to screen, but for now I used alert

--- script.js
+++ script.js
@@ -1,5 +1,85 @@
 function determine(){
- var opt = document.feeling.option1.value;
+
+
+var choice = document.getElementsByName("choice");
+    if(choice[0].checked){
+        alert("Blue Lace Agate, Stone of Encouragement!");
+    }
+    if (choice[1].checked){
+        alert("Black Tourmaline cleanses bad energy!")
+    }
+    if (choice[2].checked){
+        alert("Citrine keeps you boosted and energized!")
+    }
+   else (choice[3].checked){
+        alert("Selenite is good for peace and purity!")
+    }
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (myFeeling == "overwhelmed"){
+    // return "Blue Lace Agate is the stone of communication. If you're feeling buried, this crystal will bring you back to the surface.";
+// }
+
+// document.getElementById("message").innerHTML = determine();
+// document.write("hhh");
+
+
+
+
+
+
+// document.getElementById("after_submit").style.visibility = "visible";
+
+
+// var x = document.getElementById("option1").value;
+// document.getElementById("message").innerHTML = x;
+
+
+
+
+/*var opt = document.feeling.option1.value;
  var points = 0;
      
     if (option1 == "overwhelmed"){
@@ -23,5 +103,4 @@ var range =
 
 
 
-document.getElementById("after_submit").style.visibility = "visible";
-};
+document.getElementById("after_submit").style.visibility = "visible"; */
